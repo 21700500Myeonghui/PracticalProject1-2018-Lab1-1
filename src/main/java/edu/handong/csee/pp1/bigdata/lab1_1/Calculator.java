708365650ae7d29ce3293c8a2406ab114fc1ce2f
21700500Myeonghui <21700500@handong.edu>
2018-10-02 21:41:42 +0900

    #2 divide code modified

--- src/main/java/edu/handong/csee/pp1/bigdata/lab1_1/Calculator.java
+++ src/main/java/edu/handong/csee/pp1/bigdata/lab1_1/Calculator.java
@@ -77,7 +77,7 @@ public class Calculator {
 	 * @param result
 	 * This method prints out computation result.
 	 */
-	void print(String prefix, int result) {
+	void print(String prefix, double result) {
 		System.out.println(prefix + " " + result);
 	}
 
@@ -99,10 +99,15 @@ public class Calculator {
 		return first * second;
 	}
 
-	int divide(int first, int second) {
+	double divide(int first, int second) {
 		countForAnyCompution++;
 		localCount++;
-		return first / second;
+
+		if(second==0)
+			return Double.NaN;
+		return (double)first/second;
+		
+
 	}
 
 	int mod(int first, int second) {
